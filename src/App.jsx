99d436d6a11e3f5602e5f1d22998246e6c15a259
Yuzuki <165975846+YzkSK@users.noreply.github.com>
2025-06-28 12:34:37 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Link } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <Link to="/PageB">Go to Page B</Link>
+      </div>
+    </>
+  )
+}
+
+
---
+++ src/PageB.jsx
@@ -0,0 +1,9 @@
+
+export function PageB() {
+  return (
+    <div>
+      <h1>Page B</h1>
+      <p>This is Page B content.</p>
+    </div>
+  );
+}
